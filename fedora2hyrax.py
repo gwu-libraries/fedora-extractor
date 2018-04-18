9ea262effd37f049526538f41f1401dfe5cf22ba
Dan Kerchner <kerchner@gwu.edu>
2018-04-18 17:16:30 -0400

    Treat every metadata field the same, as potential multi-value

--- fedora2hyrax.py
+++ fedora2hyrax.py
@@ -1,6 +1,19 @@
 import json
 import sys
 
+# Fedora to Hyrax mapping
+f2h = {
+    'title': 'http://purl.org/dc/terms/title',
+    'resource_type': 'http://purl.org/dc/terms/type',
+    'creator': 'http://purl.org/dc/terms/creator',
+    'keyword': 'http://purl.org/dc/terms/relation',
+    'description': 'http://purl.org/dc/terms/description',
+    'date_created': 'http://purl.org/dc/terms/created',
+    'gw_affiliation': 'http://scholarspace.library.gwu.edu/ns#gwaffiliation',
+    'language': 'http://purl.org/dc/terms/language',
+    'rights': 'http://purl.org/dc/terms/rights'
+}
+
 
 def fedora2hyrax():
     filename = sys.argv[1]
@@ -8,18 +21,9 @@ def fedora2hyrax():
         j = json.loads(f.read())
         # print(json.dumps(j, indent=4, sort_keys=True))
         j2 = {}
-        j2['title'] = j['http://purl.org/dc/terms/title'][0]['@value']
-        j2['resource_type'] = j['http://purl.org/dc/terms/type'][0]['@value']
-        j2['creator'] = j['http://purl.org/dc/terms/creator'][0]['@value']
-        if 'http://purl.org/dc/terms/relation' in j:
-            j2['keyword'] = [k['@value'] for k in j['http://purl.org/dc/terms/relation']]
-        j2['description'] = j['http://purl.org/dc/terms/description'][0]['@value']
-        j2['date_created'] = j['http://purl.org/dc/terms/created'][0]['@value']
-        if 'http://scholarspace.library.gwu.edu/ns#gwaffiliation' in j:
-            j2['gw_affiliation'] = j['http://scholarspace.library.gwu.edu/ns#gwaffiliation'][0]['@value']
-        if 'http://purl.org/dc/terms/language' in j:
-            j2['language'] = j['http://purl.org/dc/terms/language'][0]['@value']
-        j2['rights'] = j['http://purl.org/dc/terms/rights'][0]['@value']
+        for fed, hyr in f2h.items():
+            if hyr in j:
+                j2[fed] = [k['@value'] for k in j[hyr]]
         print(json.dumps(j2, indent=4, sort_keys=True))
 
 
